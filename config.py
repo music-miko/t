a3e7d24e91b12832226494b530457e8bba7c0b89
Team DeadlineTech <tosu80018@gmail.com>
2026-02-24 20:42:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 120))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
-MEDIA_CHANNEL_ID = int(getenv("LOGGER_ID", None))
+MEDIA_CHANNEL_ID = int(getenv("MEDIA_CHANNEL_ID", None))
 
 # Get this value from @Harry_RoxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6848223695))
