294bce238fcf0cad4adc2ff6fd2fcf24d581b0f5
music-miko <voilet856@gmail.com>
2026-02-25 01:56:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,13 +19,15 @@ API_KEY = getenv("API_KEY")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+DB_URI = getenv("DB_URI", None)
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 120))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
+MEDIA_CHANNEL_ID = int(getenv("MEDIA_CHANNEL_ID", None))
 
-# Get this value from @Harry_RoxBot on Telegram by /id
+# Get this value from @DazaiiRoBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6848223695))
 
 ## Fill these variables if you're deploying on heroku.
